a3c4dd83cf3076324fa310c0dfe3889f1afbed0b
Cllic <cllic.cooperation@gmail.com>
2026-03-19 01:20:07 +0300

    Update decoder_compressed.py
    Removed AI's crappy description.

--- decoder_compressed.py
+++ decoder_compressed.py
@@ -3,31 +3,25 @@ from random import choice
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 
 def decrypt(cipher, cod_key):
-    # 1. Восстановление ключа
     try:
         shift = int(cod_key[-1])
         pure = cod_key[:-1]
         key = pure[-shift:] + pure[:-shift]
     except: return "ОШИБКА КЛЮЧА"
 
-    # 2. Подготовка текста
     work_word = cipher[::-1]
     res = []
     i = k_ptr = 0
 
-    # 3. Цикл дешифровки
     while i < len(work_word):
         k_char = key[k_ptr % len(key)]
 
         if k_char.isdigit():
-            # Цезарь назад
             idx = ALPHABET.index(work_word[i])
             res.append(ALPHABET[(idx - int(k_char)) % len(ALPHABET)])
             i += 1
         else:
-            # Разница индексов
             k_val = ALPHABET.index(k_char)
-            # Проверяем, число ли перед нами (2 знака или 1)
             if work_word[i:i+2].isdigit():
                 diff, i = int(work_word[i:i+2]), i + 2
             elif work_word[i].isdigit():
@@ -41,7 +35,6 @@ def decrypt(cipher, cod_key):
 
     return "".join(res)
 
-# Тест
 cipher = input("Шифрограмма: ")
 key = input("Ключ: ")
 print(f"Результат: {decrypt(cipher, key)}")
